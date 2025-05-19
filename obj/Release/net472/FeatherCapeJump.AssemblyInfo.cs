420c297d8f4ebfd7d498351758e4e9e717f04f49
SHIVAM PRASAD <shivam13juna@gmail.com>
2025-05-19 09:36:12 +0530

    Adding release push, and adding workflow for releasing

--- obj/Release/net472/FeatherCapeJump.AssemblyInfo.cs
+++ obj/Release/net472/FeatherCapeJump.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeatherCapeJump")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115c98b4285f853efff62acd1ac7aba2397d0ce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeatherCapeJump")]
